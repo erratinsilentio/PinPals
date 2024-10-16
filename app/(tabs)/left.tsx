0ccb78b8773154d9193ac6ebf122957f9352e637
erratinsilentio <kacper.kondracki@wp.pl>
2024-10-16 14:51:36 +0200

    added one more tab to represent three main parts of the application

--- app/(tabs)/left.tsx
+++ app/(tabs)/left.tsx
@@ -3,16 +3,16 @@ import { StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-export default function TabCenterScreen() {
+export default function TabLeftScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Map</Text>
+      <Text style={styles.title}>Friends</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
